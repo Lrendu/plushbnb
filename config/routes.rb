97e9fd165dc1c2d0976c62248191d6e6210be601
amelie darnat <darnat.amelie@gmail.com>
2021-11-16 13:10:16 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'plushes#index'
-  resources :users, only: [:new, :create]
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :plushes, only: [:new, :create, :index, :show]
-  resources :rentals, only: [:new, :create]
+  # resources :plushes, only: [:new, :create, :index, :show]
+  # resources :rentals, only: [:new, :create]
+end
