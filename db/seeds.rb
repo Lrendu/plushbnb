a38418e338f23cc03a8d1d13883fd59ed755d8b5
anne rita de livonniere <anneritadelivonniere@gmail.com>
2021-11-16 13:32:18 +0100

    seeds v3

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,63 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Plush.destroy_all
+Rental.destroy_all
 
 alex = User.create!(
   name: 'Alex',
-  description: 'Les peluches, c\'est ma vie'
-  email:    'alex@gmail.com',
+  description: 'Les peluches, c\'est ma vie',
+  email: 'alex@gmail.com',
   password: 'azertyu123'
 )
 
 joseph = User.create!(
   name: 'Joseph',
-  description: 'La peluche, y a que ça de vrai!'
-  email:    'jojo@gmail.com',
+  description: 'La peluche, y a que ça de vrai!',
+  email: 'jojo@gmail.com',
   password: 'ghcjgfh3569'
 )
 
 eleonore = User.create!(
   name: 'Eleonore',
-  description: 'Les bisounours me font rêver!'
-  email:    'leo@gmail.com',
+  description: 'Les bisounours me font rêver!',
+  email: 'leo@gmail.com',
   password: 'lugjhg87654'
 )
 
-joseph = User.create!(
-  name: 'Joseph',
-  description: 'La peluche, y a que ça de vrai!'
-  email:    'jojo@gmail.com',
-  password: 'ghcjgfh3569'
+betty = User.create!(
+  name: 'Betty',
+  description: 'Mieux qu\'une peluche : deux peluches!',
+  email: 'bet@gmail.com',
+  password: 'gjhhg3567'
 )
+puts "#{User.count} users were created"
 
-Plush.create!(
+brie = Plush.create!(
   name: 'Brie',
   description: 'La peluche Brie vous accompagne partout pour vous rappeler que le fromage c\'est la vie!',
   price: 15.25,
   owner: alex
 )
 
-Plush.create!(
+ketchup = Plush.create!(
   name: 'Ketchup',
   description: 'La peluche Ketchup vous permet d\'assaisonner vos moments moins up',
   price: 12.32,
   owner: joseph
 )
+puts "#{Plush.count} plushes were created"
 
 Rental.create!(
-  date:
-
+  date: DateTime.strptime("15/12/2021"),
+  tenant: eleonore,
+  plush: brie
 )
 
-puts "#{User.count} users were created"
-puts "#{Restaurant.count} restaurants were created"
+Rental.create!(
+  date: DateTime.strptime("05/01/2022"),
+  tenant: betty,
+  plush: ketchup
+)
+
+puts "#{Rental.count} rentals were created"
