b4f3b96565c083c44a82f85e9c4db4ad25a17552
Louise Rendu <rendu.louise@gmail.com>
2021-11-16 17:20:51 +0100

    Possibilité de supprimer une peluche

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :plushes, only: [:new, :create, :index, :show]
+  resources :plushes, only: [:new, :create, :index, :show, :destroy]
   resources :rentals, only: [:new, :create]
 end
