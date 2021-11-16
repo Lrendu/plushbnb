eeabb03070fd92acfd02dc5a133c06d8a49049a8
Jordan Gombert <jordiland18@hotmail.com>
2021-11-16 13:14:29 +0100

    In progress

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,12 @@
 class RentalsController < ApplicationController
   def create
+    @rental = Rental.new(rental_params)
+    @rental.save
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:date, :tenant_id, :plush_id)
   end
 end
