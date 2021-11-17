9c1a212c5bb46a671e5615992185f7efd0bfb037
Jordan Gombert <jordiland18@hotmail.com>
2021-11-17 10:14:14 +0100

    User profile + User plushes pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/user_profile', to: 'users#user_profile'
   get 'users/user_plushes', to: 'users#user_plushes'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'plushes#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :plushes, only: [:new, :create, :index, :show]
   resources :rentals, only: [:new, :create]
