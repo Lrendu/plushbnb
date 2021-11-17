65a5ee6ed217199ff09896b1d17ebb2074b84a9a
Jordan Gombert <jordiland18@hotmail.com>
2021-11-17 15:49:11 +0100

    Fixes bug on config css

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,4 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 const initFlatpickr = () => {
   const rentalForm = document.getElementById('new_rental');
