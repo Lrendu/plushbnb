13b0626cb6fe7074f4121667d613db564af4e1b7
Louise Rendu <rendu.louise@gmail.com>
2021-11-17 13:47:10 +0100

    Pundit réparé ! Create New Destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def user_profile
+    @user = current_user
+    authorize @user
   end
 
   def user_plushes
+    @user = current_user
+    authorize @user
   end
 end
